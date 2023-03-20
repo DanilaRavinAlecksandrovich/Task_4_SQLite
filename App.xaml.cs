58da4df8126fe03a00a9effac617ee009f718355
IRU <IRU@WIN-B2H1FLRQ82T>
2023-03-20 22:31:07 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Task_4_SQLite.Model;
+
+namespace Task_4_SQLite
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnActivated(EventArgs e)
+        {
+            DatabaseFacade facade = new DatabaseFacade(new UserDataContext());
+            facade.EnsureCreated();
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Task_4_SQLite.Model
+{
+    public class User
+    {
+        [Key]
+
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public string UID { get; set; }
+    }
+}
